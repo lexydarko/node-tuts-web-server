1755c530f2ad8a595a0848c458f5c94c2beb5af4
Kweku Darko <kdarko@kdarko.com>
2020-05-08 14:33:28 -0500

    Display additional forcast information

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -2,16 +2,14 @@ const request = require('request')
 
 const forecast = (latitude, longitude, callback) => {
     const url = 'http://api.weatherstack.com/current?access_key=5ce733a8f5d292a9c89150e0caa494a8&query=' + latitude + ',' + longitude
-    // const url = 'https://api.darksky.net/forecast/9d1465c6f3bb7a6c71944bdd8548d026/' + latitude + ',' + longitude
 
     request({ url, json: true }, (error, { body }) => {
-        console.log(body)
         if (error) {
             callback('Unable to connect to weather service!', undefined)
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, 'The weather is ' + body.current.weather_descriptions + ' and It is currently ' + body.current.temperature + ' degress out. There is a ' + body.current.precip + '% chance of rain.')
+            callback(undefined, 'At ' +  body.current.observation_time + ' the weather is ' + body.current.weather_descriptions + ' and It is currently ' + body.current.temperature + ' degress out. There is a ' + body.current.precip + '% chance of rain.' )
         }
     })
 }
